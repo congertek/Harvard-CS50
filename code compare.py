5ae7e291b77f7b7874edb99ae2ff5057929456ad
Kelly Conger <OldManCoder@CongerTek.com>
2024-07-07 15:04:20 -0400

    init commit

--- code compare.py
+++ code compare.py
@@ -1,9 +1,18 @@
+import os
+
+def clear_terminal():
+    # Clear the terminal screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Clear the screen
+clear_terminal()
+
 x = int(input("What is x? "))
 y = int(input("What is x? "))
 
 if x < y:
-    [print("x is less than y")]
-if x > y:
+    print("x is less than y")
+elif x > y:
     print("x is greater than y")
-if x == y:
+elif x == y:
     print("x is equal to y")
