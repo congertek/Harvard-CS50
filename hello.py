22a52688e000280087df537ba302f0fc9cb1d5d7
Kelly Conger <OldManCoder@CongerTek.com>
2024-07-07 12:39:05 -0400

    211

--- hello.py
+++ hello.py
@@ -10,10 +10,13 @@ clear_terminal()
 #Ask user for their name, Include Capitalize and Remove Whitespace
 name = input("What is your name? ").strip().title()
 
+#Split user's name into first and last
+first, last = name.split(" ")
+
 # Capitalize user's name
 #name = name.strip().title()
 
 # Remove whitespace from name
 #name = name.title()
 
-print(f"Hello, {name}","|")
+print(f"Hello, {first} {last}","|\n\n\n\n\n")
