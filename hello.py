86a6531286a9811af07807db592347ae0bda405b
Kelly Conger <OldManCoder@CongerTek.com>
2024-07-07 12:22:47 -0400

    MORE STUFF

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello, world")
+import os
+import time
+
+def clear_terminal():
+    # Clear the terminal screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Clear the screen
+clear_terminal()
+
+name = input("What is your name? ")
+print(f"Hello, {name}")
