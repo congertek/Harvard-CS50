80a1d0aa65b492b3dc925ded01556cc40ab60e3a
Kelly Conger <OldManCoder@CongerTek.com>
2024-07-07 17:35:42 -0400

    aaaa

--- grades2.py
+++ grades2.py
@@ -6,15 +6,17 @@ def clear_terminal():
 # Clear the screen
 clear_terminal()
 
-score = int(input("Score: "))
+def main():
+    x = int(input("What is X? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-if score >= 90 and score <= 100:
-    print("Grade: A")
-elif score >= 80 and score < 90:
-    print("Grade: B")
-elif score >= 70 and score < 80:
-    print("Grade: C")
-elif score >= 60 and score < 70:
-    print("Grade: D")
-else:
-    print("Grade: F")
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+main()
+
